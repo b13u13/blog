2ae5739119bb1798bbb37bb623cc1341173ac7e5
b13u13 <b13u13@gmail.com>
2015-02-05 22:55:22 +0200

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Blog')
     end
+
+    it "should have he title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Home')
+    end
   end
 
   describe "Help page" do
@@ -16,6 +21,11 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Blog | Help")
+    end
   end
 
   describe "About page" do
@@ -23,7 +33,12 @@ describe "Static pages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
-    end
+
   end
 
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Blog | About Us")
+    end
+  end
 end
