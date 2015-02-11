33d655a78f65f1eef5918070463ccd19e15bc9e8
Olexandr Ovsiychuk <oovsiychuk@productengine.com>
2015-02-11 13:45:08 +0200

    Add SSL and registrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome my little tractorist"
       redirect_to @user
     else
       render 'new'
