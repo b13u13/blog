090e5774247a238482cc39e57da5d2d0a8cf4d8d
Olexandr Ovsiychuk <oovsiychuk@productengine.com>
2015-02-25 12:34:39 +0200

    S3 setup

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,4 +18,6 @@ CarrierWave.configure do |config|
   # To let CarrierWave work on heroku
   config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.fog_directory = ENV['S3_BUCKET']
+  config.fog_public = false
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
