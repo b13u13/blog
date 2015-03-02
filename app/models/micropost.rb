5d4916d42992e40e3c637161b841028c0748abb7
Olexandr Ovsiychuk <oovsiychuk@productengine.com>
2015-03-02 21:58:51 +0200

    add micropost image

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,9 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
+  mount_uploader :picture, PictureUploader
+  validate  :picture_size
+
 
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
@@ -11,5 +14,13 @@ class Micropost < ActiveRecord::Base
           user_id: user.id)
   end
 
+  private
+
+  # Validates the size of an uploaded picture.
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 
 end
---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+
+
+  def store_dir
+    'picture/'
+  end
+
+
+
+  version :thumb do
+    process :resize_to_limit => [400, 400]
+  end
+
+
+
+  def cache_dir
+    '/tmp/cache'
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+
+
+end
