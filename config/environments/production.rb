c3ebdfecfa8545567475efed1649da4164cafdfb
Olexandr Ovsiychuk <oovsiychuk@productengine.com>
2015-03-04 12:39:05 +0200

    Finish emails-5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :domain => 'whispering-inlet-2278.herokuapp.com',
+      :domain => "gmail.com",
       :user_name => "aovsiychuk@gmail.com",
       :password => "246204373",
       :authentication => :plain,
